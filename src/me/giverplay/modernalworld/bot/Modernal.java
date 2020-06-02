1ecf056ae014de3cc294a425fe540198ada42611
GiverPlay007 <play.giver.007@gmail.com>
2020-06-01 22:10:36 -0300

    1.0.0 | Login via argumento

--- src/me/giverplay/modernalworld/bot/Modernal.java
+++ src/me/giverplay/modernalworld/bot/Modernal.java
@@ -2,13 +2,35 @@ package me.giverplay.modernalworld.bot;
 
 import me.giverplay.modernalworld.bot.command.CommandManager;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
 
 public class Modernal
 {
 	private static JDA jda;
-	
+	private static String token = "null";
+
 	public static void main(String[] args)
 	{
+		try
+		{
+			jda = new JDABuilder()
+							.setToken(token)
+							.build()
+							.awaitReady();
+		}
+		catch (LoginException e)
+		{
+			System.out.println("Erro de login: Token fornecido é inválido ou a conexão está limitada");
+			System.exit(0);
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("Programa foi forçado a fechar (InterrupedException)");
+			System.exit(0);
+		}
+
 		new Modernal();
 	}
 	
